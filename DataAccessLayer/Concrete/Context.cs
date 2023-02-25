d290259347cf567e5cfd0f2682cf7b48a3020ed4
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-02-25 16:52:26 +0300

    Add context class in DataAccessLayer and Make web config

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        //        About : projedeki class ın ismi
+        //        Abouts :  sqlde veritabanımıza yansıyacak isim
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts  { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings  { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+
+        //sqldeki dbye yansıyacak kısımlar
+    }
+}
