ca3e2946cffeabd1c2432726dc8d2535203fee80
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-02-28 17:34:39 +0300

    Add CategoryController and Bootstrap. Listing

---
+++ MVC_web_app/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_web_app.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList() 
+        {
+            var categoryValues = cm.GetAllBL();
+            return View(categoryValues);
+        }
+    }
+}
