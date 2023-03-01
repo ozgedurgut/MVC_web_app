84fef895823593b08c96803d0df0db6b5570043b
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-03-01 16:37:44 +0300

    AddCategory, Abstract, HttpGet HttpPost, Service

--- MVC_web_app/Controllers/CategoryController.cs
+++ MVC_web_app/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Concrete;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,8 +18,21 @@ namespace MVC_web_app.Controllers
         }
         public ActionResult GetCategoryList() 
         {
-            var categoryValues = cm.GetAllBL();
-            return View(categoryValues);
+           // var categoryValues = cm.GetAllBL();
+            return View();
+        }
+        
+        [HttpGet]
+        public ActionResult AddCategory() 
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+          //  cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
