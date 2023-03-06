347c1d55b36a951a63609c5f77b3c6bd1b9c89f2
Ozge Hulya Durgut <ozgehulyadurgut@gmail.com>
2023-03-06 14:04:52 +0300

    Fluent Validation

---
+++ BusinessLayer/ValidationRules/CategoryValidatior.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidatior:AbstractValidator<Category>
+    {
+        public CategoryValidatior()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Category name cannot be empty");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Category description cannot be empty");
+            RuleFor(x=> x.CategoryName).MinimumLength(3).WithMessage("Category name cannot be less than 3");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Please write a name shorter than 20 characters");
+        }
+    }
+}
--- EntityLayer/Concrete/Category.cs
+++ EntityLayer/Concrete/Category.cs
@@ -17,5 +17,7 @@ namespace EntityLayer.Concrete
         public string CategoryDescription { get; set; }
         public bool CategoryStatus { get; set; }
 
+        public ICollection<Heading> Headings { get; set; }
+
     }
 }
